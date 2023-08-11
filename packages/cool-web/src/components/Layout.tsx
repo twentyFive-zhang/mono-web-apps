1199f4d1390bf97b5bda08b3b16e70f07d9e1b57
二五 <1097983662@qq.com>
2023-08-11 13:28:38 +0800

    feat: progress

--- packages/cool-web/src/components/Layout.tsx
+++ packages/cool-web/src/components/Layout.tsx
@@ -1,7 +1,8 @@
 import React, { memo, useEffect, useState } from 'react';
 import { useTranslation } from 'next-i18next';
-import { Affix, FloatButton } from 'antd';
+import { Affix, FloatButton, Spin, Progress } from 'antd';
 import { useRouter } from 'next/router';
+import Head from 'next/head';
 
 const tabList = [
   'Home',
@@ -15,17 +16,58 @@ const tabList = [
 function Layout({ children }: { children: React.ReactElement }) {
   const { t } = useTranslation(['common']);
   const router = useRouter();
-  // console.log({ router });
+  // console.log(router);
   const [chosenKey, setChosenKey] = useState<string>(tabList[0]);
 
   useEffect(() => {
     const [key] = router.pathname.split('/').reverse();
-    console.log(key);
+    // console.log(key);
     setChosenKey(key);
   }, [router.pathname]);
 
+  const [percent, setPercent] = useState<number>(0);
+  // const [hideProgress, setHideProgress] = useState<boolean>(false);
+
+  useEffect(() => {
+    const handleChangeStart = (url: string) => {
+      setPercent(10);
+    };
+
+    const handleChangeComplete = (url: string) => {
+      setPercent(100);
+      const timer = setTimeout(() => {
+        setPercent(0);
+        clearTimeout(timer);
+      }, 800);
+    };
+
+    router.events.on('routeChangeStart', handleChangeStart);
+    router.events.on('routeChangeComplete', handleChangeComplete);
+    router.events.on('routeChangeError', handleChangeComplete);
+
+    return function cleanup() {
+      router.events.off('routeChangeStart', handleChangeStart);
+      router.events.off('routeChangeComplete', handleChangeComplete);
+      router.events.off('routeChangeError', handleChangeComplete);
+    };
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
+
   return (
     <div>
+      <Head>
+        <title>{t(chosenKey)}</title>
+      </Head>
+      <div
+        className={`fixed top-[-10px] z-30 w-full transition delay-0 opacity-${percent}`}
+      >
+        <Progress
+          showInfo={false}
+          strokeColor={{ from: '#108ee9', to: '#87d068' }}
+          percent={percent}
+          trailColor="white"
+        ></Progress>
+      </div>
       <Affix>
         <div className="px-4 lg:px-8 py-8 shadow-lg  text-black bg-white bg-white/80 backdrop-blur backdrop-filter sticky top-0 z-10 lg:py-6">
           <div className="mx-auto max-w-screen-2xl  px-4 flex">
